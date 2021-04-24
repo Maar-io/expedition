496501417d71f9cf3db8a33926b9a2f166ce7fcb
Mario <mariovukelic@yahoo.com>
2021-04-24 19:44:45 +0200

    Updates

---
+++ precache-manifest.5b3eec356ff2f5b5beca66580d090f57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ebb2829a53262703cc020a8820ee17",
+    "url": "/Maar-io/expedition/index.html"
+  },
+  {
+    "revision": "794f50e9c81138fe437f",
+    "url": "/Maar-io/expedition/static/css/main.14686620.chunk.css"
+  },
+  {
+    "revision": "ac1ae81321ad2c027fc0",
+    "url": "/Maar-io/expedition/static/js/2.39620a47.chunk.js"
+  },
+  {
+    "revision": "aaf168b94062fd5eddc2bafce074e7d6",
+    "url": "/Maar-io/expedition/static/js/2.39620a47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "794f50e9c81138fe437f",
+    "url": "/Maar-io/expedition/static/js/main.7bfa0e33.chunk.js"
+  },
+  {
+    "revision": "5c36c76b4bf56d3e5b1a",
+    "url": "/Maar-io/expedition/static/js/runtime-main.a907c3dc.js"
+  }
+]);
